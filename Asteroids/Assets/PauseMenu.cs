1be5d81d10b0c4f1be8f99cd7e6441f16c7e20da
Sparky <dk79@zips.uakron.edu>
2019-09-25 18:12:12 -0400

    GUI Update - Pause Menu
    Added Fading Animation
    Added "Esc" toggle Keybind
    Added Quit Functionality (Will work when built)
    
    (Need to add MainMenu button and Main Menu Window.)

---
+++ Asteroids/Assets/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Update is called once per frame
+
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        IsGamePaused = true;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu test.");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+
+
+
+
+
+}
